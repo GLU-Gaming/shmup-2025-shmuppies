5d22825804b439022e30e95dde6b5cff1aede7cc
jopnot <Jopklein4@gmail.com>
2025-03-24 10:26:28 +0100

    values type shit

--- Assets/_Project/Scripts/EnemyScripts/enemybehavior1.cs
+++ Assets/_Project/Scripts/EnemyScripts/enemybehavior1.cs
@@ -1,18 +1,21 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class enemymovement : MonoBehaviour
 {
+    public GameObject player;
+   
     private Rigidbody rb;
-    [SerializeField] private float forwardspeed;
+    public float speed = 4f;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        rb.linearVelocity= transform.forward*forwardspeed;
+        
     }
 
  
-    void Update()
+    void FixedUpdate()
     {
-        
+        gameObject.transform.LookAt(player.transform.position, Vector3.up);
+        rb.position = Vector3.MoveTowards(gameObject.transform.position, player.transform.position, speed * Time.fixedDeltaTime);
     }
 }
